c7d30cbc998999cc70cdae47287692bdfa6865dc
junepaik <ant.web3@gmail.com>
2015-07-30 17:42:31 +0300

    Minor code format fixes

--- www/engine/Framework/Classes/Geo/Utils/Lister.php
+++ www/engine/Framework/Classes/Geo/Utils/Lister.php
@@ -8,45 +8,45 @@ namespace Geo\Utils {
 
 		private static $list = array();
 
-        # Load list
+		# Load list
 
-        protected static function init($file_name) {
+		protected static function init($file_name) {
 
-            self::$list = Explorer::php(DIR_DATA . $file_name);
-        }
+			self::$list = Explorer::php(DIR_DATA . $file_name);
+		}
 
-        # Check if item exists
+		# Check if item exists
 
-        public static function exists($key) {
+		public static function exists($key) {
 
-            $key = String::validate($key);
+			$key = String::validate($key);
 
-            return (isset(self::$list[$key]));
-        }
+			return (isset(self::$list[$key]));
+		}
 
-        # Validate key
+		# Validate key
 
-        public static function validate($key) {
+		public static function validate($key) {
 
-            $key = String::validate($key);
+			$key = String::validate($key);
 
-            return (self::exists($key) ? $key : false);
-        }
+			return (self::exists($key) ? $key : false);
+		}
 
-        # Get item by key
+		# Get item by key
 
-        public static function get($key) {
+		public static function get($key) {
 
-            $key = String::validate($key);
+			$key = String::validate($key);
 
-            return (self::exists($key) ? self::$list[$key] : false);
-        }
+			return (self::exists($key) ? self::$list[$key] : false);
+		}
 
-        # Get list
+		# Get list
 
-        public static function range() {
+		public static function range() {
 
-            return self::$list;
-        }
+			return self::$list;
+		}
 	}
 }
