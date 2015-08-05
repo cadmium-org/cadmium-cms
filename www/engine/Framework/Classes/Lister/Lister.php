518f7588453e3fe50cfb504ea8a1f05d44c784cd
junepaik <ant.web3@gmail.com>
2015-08-06 01:48:27 +0300

    Updated Lister class

--- www/engine/Framework/Classes/Lister/Lister.php
+++ www/engine/Framework/Classes/Lister/Lister.php
@@ -10,43 +10,43 @@ namespace {
 
 		protected static function init($file_name) {
 
-			$file_name = (DIR_DATA . $file_name);
+			$file_name = (DIR_DATA . $file_name); $class = get_called_class();
 
-			if (is_array($list = Explorer::php($file_name))) self::$list = $list;
+			if (is_array($list = Explorer::php($file_name))) self::$list[$class] = $list;
 		}
 
 		# Check if item exists
 
 		public static function exists($key) {
 
-			$key = strval($key);
+			$key = strval($key); $class = get_called_class();
 
-			return isset(self::$list[$key]);
+			return isset(self::$list[$class][$key]);
 		}
 
 		# Validate key
 
 		public static function validate($key) {
 
-			$key = strval($key);
+			$key = strval($key); $class = get_called_class();
 
-			return (isset(self::$list[$key]) ? $key : false);
+			return (isset(self::$list[$class][$key]) ? $key : false);
 		}
 
 		# Get item by key
 
 		public static function get($key) {
 
-			$key = strval($key);
+			$key = strval($key); $class = get_called_class();
 
-			return (isset(self::$list[$key]) ? self::$list[$key] : false);
+			return (isset(self::$list[$class][$key]) ? self::$list[$class][$key] : false);
 		}
 
 		# Get list
 
 		public static function range() {
 
-			return self::$list;
+			return self::$list[get_called_class()];
 		}
 	}
 }
