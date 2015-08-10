e23f4ed014f0d1eff33cf4519424f6e5822a0b9b
junepaik <ant.web3@gmail.com>
2015-08-10 17:48:59 +0300

    Minor optimizations

--- www/engine/System/Classes/Utils/Entity/Type/User/Implementor.php
+++ www/engine/System/Classes/Utils/Entity/Type/User/Implementor.php
@@ -2,7 +2,18 @@
 
 namespace System\Utils\Entity\Type\User {
 
+	use String;
+
 	class Implementor extends Definition {
+
+		# Validate param
+
+		private static function validate($string, $regex, $min, $max) {
+
+			if (!preg_match($regex, $string)) return false;
+
+			return ((String::between($string, $min, $max)) ? $string : false);
+		}
 
         # Implement entity
 
@@ -17,13 +28,10 @@ namespace System\Utils\Entity\Type\User {
 
 			$name = strval($name);
 
-			if (!preg_match(REGEX_USER_NAME, $name)) return false;
-
 			$min = CONFIG_USER_NAME_MIN_LENGTH; $max = CONFIG_USER_NAME_MAX_LENGTH;
 
-			# ------------------------
+			return self::validate($name, REGEX_USER_NAME, $min, $max);
 
-			return (preg_match(('/^(?=.{' . $min . ',' . $max . '}$).+$/'), $name) ? $name : false);
 		}
 
 		# Validate password
@@ -32,13 +40,9 @@ namespace System\Utils\Entity\Type\User {
 
 			$password = strval($password);
 
-			if (!preg_match(REGEX_USER_PASSWORD, $password)) return false;
-
 			$min = CONFIG_USER_PASSWORD_MIN_LENGTH; $max = CONFIG_USER_PASSWORD_MAX_LENGTH;
 
-			# ------------------------
-
-			return (preg_match(('/^(?=.{' . $min . ',' . $max . '}$).+$/'), $password) ? $password : false);
+			return self::validate($password, REGEX_USER_PASSWORD, $min, $max);
 		}
 	}
 }
