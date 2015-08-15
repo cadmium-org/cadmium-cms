6a1ea212375010429c004c57302693574d2e6efb
junepaik <ant.web3@gmail.com>
2015-08-15 15:16:06 +0300

    Updated dependencies

--- www/engine/System/Classes/Handlers/Site/Profile/Overview.php
+++ www/engine/System/Classes/Handlers/Site/Profile/Overview.php
@@ -2,9 +2,9 @@
 
 namespace System\Handlers\Site\Profile {
 
-	use Error, System, System\Forms, System\Views, System\Utils\Ajax, System\Utils\Auth, System\Utils\Config;
+	use Error, System, System\Forms, System\Utils\Ajax, System\Utils\Auth, System\Utils\Config;
 	use System\Utils\Entity, System\Utils\Extend, System\Utils\Lister, System\Utils\Messages;
-	use System\Utils\Pagination, System\Utils\Requirements, System\Utils\Utils;
+	use System\Utils\Pagination, System\Utils\Requirements, System\Utils\Utils, System\Utils\View;
 
 	use Agent, Arr, Cookie, Date, DB, Explorer, Form, Geo\Country, Geo\Timezone;
 	use Headers, Language, Mailer, Number, Request, Session, String, Tag, Template, Url, Validate;
