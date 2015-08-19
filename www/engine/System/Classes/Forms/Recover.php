d481a6ada448257beae19ef4962b5542998b8dc1
junepaik <ant.web3@gmail.com>
2015-08-19 21:08:53 +0300

    Extracted Recover handler class

--- www/engine/System/Classes/Forms/Recover.php
+++ www/engine/System/Classes/Forms/Recover.php
@@ -2,7 +2,7 @@
 
 namespace System\Forms {
 
-	use System\Utils\Auth, System\Utils\Messages, Form, Language;
+	use Form, Language;
 
 	class Recover extends Form {
 
@@ -23,22 +23,6 @@ namespace System\Forms {
 			$this->input('password_retype', '', FORM_INPUT_PASSWORD, CONFIG_USER_PASSWORD_MAX_LENGTH,
 
                 ($placeholder ? Language::get('USER_FIELD_PASSWORD_RETYPE') : ''), FORM_FIELD_REQUIRED);
-        }
-
-        # Handle form
-
-        public function handle() {
-
-            if (false !== ($post = $this->post())) {
-
-				if ($this->errors()) Messages::error(Language::get('FORM_ERROR_REQUIRED'));
-
-				else if (true !== ($result = Auth\Controller::recover($post))) Messages::error(Language::get($result));
-
-				else return true;
-			}
-
-            return false;
         }
     }
 }
--- www/engine/System/Classes/Handlers/Admin/Auth/Recover.php
+++ www/engine/System/Classes/Handlers/Admin/Auth/Recover.php
@@ -2,12 +2,7 @@
 
 namespace System\Handlers\Admin\Auth {
 
-	use Error, System, System\Forms, System\Utils\Ajax, System\Utils\Auth, System\Utils\Config;
-	use System\Utils\Entitizer, System\Utils\Extend, System\Utils\Lister, System\Utils\Messages;
-	use System\Utils\Pagination, System\Utils\Requirements, System\Utils\Utils, System\Utils\View;
-
-	use Agent, Arr, Cookie, Date, DB, Explorer, Form, Geo\Country, Geo\Timezone;
-	use Headers, Language, Mailer, Number, Request, Session, String, Tag, Template, Url, Validate;
+	use System, System\Forms, System\Utils\Auth, Language, Request;
 
 	class Recover extends System\Frames\Admin\Component\Auth {
 
@@ -17,27 +12,11 @@ namespace System\Handlers\Admin\Auth {
 
 			if ($this->initial()) Request::redirect('/admin/register');
 
-			if (false === ($code = Auth::secret(true))) Request::redirect('/admin/reset');
-
-			# Create form
-
-			$form = new Forms\Recover(true);
-
-			if ($form->handle()) Request::redirect('/admin/login?submitted=recover');
-
-			# Create contents block
-
-			$contents = View::get('Blocks/Contents/Auth/Recover');
-
-			$contents->code = $code;
-
-			$form->implement($contents);
-
 			# Fill template
 
 			$this->setTitle(Language::get('TITLE_AUTH_RECOVER'));
 
-			$this->setContents($contents);
+			$this->setContents(Auth\Handler\Recover::handle(true));
 
 			# ------------------------
 
