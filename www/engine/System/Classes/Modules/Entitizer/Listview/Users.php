72e8c04c54dbb5e0481b2e8763466cef0c1c38e2
Anton Romanov <ant.web3@gmail.com>
2015-09-15 00:51:57 +0300

    Scrutinizer test

--- www/engine/System/Classes/Modules/Entitizer/Listview/Users.php
+++ www/engine/System/Classes/Modules/Entitizer/Listview/Users.php
@@ -10,29 +10,29 @@ namespace System\Modules\Entitizer\Listview {
 		use Entitizer\Utils\Lister;
 		use Entitizer\Utils\Listview;
 
-		private static $link = '/admin/system/users';
+		protected static $link = '/admin/system/users';
 
-		private static $naming = 'name';
+		protected static $naming = 'name';
 
-		private static $select = array('rank', 'name'), $order_by = array('rank DESC', 'name ASC');
+		protected static $select = array('rank', 'name'), $order_by = array('rank DESC', 'name ASC');
 
-		private static $display = CONFIG_ADMIN_USERS_DISPLAY;
+		protected static $display = CONFIG_ADMIN_USERS_DISPLAY;
 
-		private static $view_main = 'Blocks\Entitizer\Users\Listview\Main';
+		protected static $view_main = 'Blocks\Entitizer\Users\Listview\Main';
 
-		private static $view_item = 'Blocks\Entitizer\Users\Listview\Item';
+		protected static $view_item = 'Blocks\Entitizer\Users\Listview\Item';
 
-		private static $view_ajax_main = '';
+		protected static $view_ajax_main = '';
 
-		private static $view_ajax_item = '';
+		protected static $view_ajax_item = '';
 
 		# Add additional data for specific entity
 
-		private static function processEntity(Template\Utils\Block $contents) {}
+		protected static function processEntity(Template\Utils\Block $contents) {}
 
 		# Add item additional data
 
-		private static function processItem(Template\Utils\Block $view, array $data) {
+		protected static function processItem(Template\Utils\Block $view, array $data) {
 
 			$view->rank = Lister\Rank::get($data['rank']);
 		}
