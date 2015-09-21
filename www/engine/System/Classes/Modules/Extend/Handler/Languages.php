c48d5c6f2e4835aecbe7db3018a1db43d736fba3
Anton Romanov <ant.web3@gmail.com>
2015-09-21 20:41:32 +0300

    Changed properties accessibility

--- www/engine/System/Classes/Modules/Extend/Handler/Languages.php
+++ www/engine/System/Classes/Modules/Extend/Handler/Languages.php
@@ -8,8 +8,8 @@ namespace System\Modules\Extend\Handler {
 
         use Extend\Utils\Handler;
 
-        private static $error_name = 'LANGUAGES_ERROR_NAME', $errors_save = 'LANGUAGES_ERROR_SAVE';
+        protected static $error_name = 'LANGUAGES_ERROR_NAME', $errors_save = 'LANGUAGES_ERROR_SAVE';
 
-        private static $view_main = 'Blocks\Extend\Languages\Main', $view_item = 'Blocks\Extend\Languages\Item';
+        protected static $view_main = 'Blocks\Extend\Languages\Main', $view_item = 'Blocks\Extend\Languages\Item';
 	}
 }
--- www/engine/System/Classes/Modules/Extend/Handler/Templates.php
+++ www/engine/System/Classes/Modules/Extend/Handler/Templates.php
@@ -8,8 +8,8 @@ namespace System\Modules\Extend\Handler {
 
         use Extend\Utils\Handler;
 
-        private static $error_name = 'TEMPLATES_ERROR_NAME', $errors_save = 'TEMPLATES_ERROR_SAVE';
+        protected static $error_name = 'TEMPLATES_ERROR_NAME', $errors_save = 'TEMPLATES_ERROR_SAVE';
 
-        private static $view_main = 'Blocks\Extend\Templates\Main', $view_item = 'Blocks\Extend\Templates\Item';
+        protected static $view_main = 'Blocks\Extend\Templates\Main', $view_item = 'Blocks\Extend\Templates\Item';
 	}
 }
