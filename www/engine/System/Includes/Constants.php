7aab825dea8a08d1f996c9f00d1103628f1b7e26
Anton Romanov <ant.web3@gmail.com>
2015-10-24 18:34:51 +0300

    Updated version to 0.1.3

--- www/engine/System/Includes/Constants.php
+++ www/engine/System/Includes/Constants.php
@@ -5,7 +5,7 @@
 define('CADMIUM_NAME',                              'Cadmium CMS');
 define('CADMIUM_HOME',                              'http://cadmium-cms.com');
 
-define('CADMIUM_VERSION',                           '0.1.2');
+define('CADMIUM_VERSION',                           '0.1.3');
 define('CADMIUM_COPY',                              '2015');
 
 # External
