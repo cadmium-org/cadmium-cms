b3a52e475db74773d830aad497081b649cd3906f
Anton Romanov <ant.web3@gmail.com>
2015-11-05 19:37:45 +0200

    Updated Cookie class

--- www/engine/Framework/Classes/Cookie/Cookie.php
+++ www/engine/Framework/Classes/Cookie/Cookie.php
@@ -6,40 +6,30 @@ namespace {
 
 		# Set variable
 
-		public static function set($name, $value, $expire = 0, $path = '/', $domain = false, $secure = false, $http_only = false) {
+		public static function set(string $name, string $value, int $expire = 0,
 
-			$name = strval($name); $value = strval($value); $expire = (REQUEST_TIME + intabs($expire));
+			string $path = '/', string $domain = '', bool $secure = false, bool $http_only = false) {
 
-			$path = strval($path); $domain = strval($domain); $secure = boolval($secure); $http_only = boolval($http_only);
-
-			# ------------------------
-
-			return setcookie($name, $value, $expire, $path, $domain, $secure, $http_only);
+			return setcookie($name, $value, (REQUEST_TIME + $expire), $path, $domain, $secure, $http_only);
 		}
 
 		# Check if variable exists
 
-		public static function exists($name) {
-
-			$name = strval($name);
+		public static function exists(string $name) {
 
 			return isset($_COOKIE[$name]);
 		}
 
 		# Get variable
 
-		public static function get($name) {
-
-			$name = strval($name);
+		public static function get(string $name) {
 
 			return (isset($_COOKIE[$name]) ? $_COOKIE[$name] : null);
 		}
 
 		# Delete variable
 
-		public static function delete($name) {
-
-			$name = strval($name);
+		public static function delete(string $name) {
 
 			if (isset($_COOKIE[$name])) unset($_COOKIE[$name]);
 		}
