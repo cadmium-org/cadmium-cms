700ba5444892424476e32d332e32bcd26660d1a9
Anton Romanov <ant.web3@gmail.com>
2015-11-05 20:26:04 +0200

    Updated Validate class

--- www/engine/Framework/Classes/Validate/Validate.php
+++ www/engine/Framework/Classes/Validate/Validate.php
@@ -6,27 +6,21 @@ namespace {
 
 		# Validate ip
 
-		public static function ip($value) {
-
-			$value = strval($value);
+		public static function ip(string $value) {
 
 			return filter_var($value, FILTER_VALIDATE_IP);
 		}
 
 		# Validate email
 
-		public static function email($value) {
-
-			$value = strval($value);
+		public static function email(string $value) {
 
 			return filter_var($value, FILTER_VALIDATE_EMAIL);
 		}
 
 		# Validate url
 
-		public static function url($value) {
-
-			$value = strval($value);
+		public static function url(string $value) {
 
 			$value = filter_var($value, FILTER_VALIDATE_URL);
 
