7c7799ddae93be1e802400b4e2c1d7b8d549bb8c
Anton Romanov <ant.web3@gmail.com>
2015-11-06 18:23:33 +0200

    Updated Explorer class

--- www/engine/Framework/Classes/Explorer/Explorer.php
+++ www/engine/Framework/Classes/Explorer/Explorer.php
@@ -6,73 +6,53 @@ namespace {
 
 		# Get list of directory items
 
-		private static function getList($dir_name, $dirs = false) {
-
-			$dir_name = strval($dir_name);
+		private static function getList(string $dir_name, bool $dirs = false) {
 
 			if (false === ($handler = @opendir($dir_name))) return [];
-
-			$list = [];
 
 			while (false !== ($name = readdir($handler))) {
 
 				if (($name === '.') || ($name === '..')) continue;
 
-				if ($dirs ? @is_dir($dir_name . $name) : @is_file($dir_name . $name)) $list[] = $name;
+				if ($dirs ? @is_dir($dir_name . $name) : @is_file($dir_name . $name)) yield $name;
 			}
 
 			closedir($handler);
-
-			# ------------------------
-
-			return $list;
 		}
 
 		# Get file info
 
-		private static function getInfo($file_name, $param, $check_exists = true) {
-
-			$file_name = strval($file_name); $check_exists = boolval($check_exists);
+		private static function getInfo(string $file_name, int $param, bool $check_exists = true) {
 
 			if ($check_exists && !self::isFile($file_name)) return false;
-
-			# ------------------------
 
 			return pathinfo($file_name, $param);
 		}
 
 		# Check if file exists
 
-		public static function isFile($file_name) {
-
-			$file_name = strval($file_name);
+		public static function isFile(string $file_name) {
 
 			return (@file_exists($file_name) && @is_file($file_name));
 		}
 
 		# Check if directory exists
 
-		public static function isDir($dir_name) {
-
-			$dir_name = strval($dir_name);
+		public static function isDir(string $dir_name) {
 
 			return (@file_exists($dir_name) && @is_dir($dir_name));
 		}
 
 		# Remove file
 
-		public static function removeFile($file_name) {
-
-			$file_name = strval($file_name);
+		public static function removeFile(string $file_name) {
 
 			return @unlink($file_name);
 		}
 
 		# Remove directory
 
-		public static function removeDir($dir_name, $recursive = false) {
-
-			$dir_name = strval($dir_name); $recursive = boolval($recursive);
+		public static function removeDir(string $dir_name, bool $recursive = false) {
 
 			if ($recursive && (false !== ($list = @scandir($dir_name)))) {
 
@@ -80,9 +60,9 @@ namespace {
 
 					$name = ($dir_name . '/' . $name);
 
-					if (@is_dir($name)) self::removeDir($name, true);
+					if (@is_file($name)) self::removeFile($name);
 
-					else if (@is_file($name)) self::removeFile($name);
+					else if (@is_dir($name)) self::removeDir($name, true);
 				}
 			}
 
@@ -93,58 +73,56 @@ namespace {
 
 		# Get files list
 
-		public static function listFiles($dir_name) {
+		public static function listFiles(string $dir_name) {
 
 			return self::getList($dir_name, false);
 		}
 
 		# Get directories list
 
-		public static function listDirs($dir_name) {
+		public static function listDirs(string $dir_name) {
 
 			return self::getList($dir_name, true);
 		}
 
 		# Get file dirname
 
-		public static function dirname($file_name, $check_exists = true) {
+		public static function dirname(string $file_name, bool $check_exists = true) {
 
 			return self::getInfo($file_name, PATHINFO_DIRNAME, $check_exists);
 		}
 
 		# Get file basename
 
-		public static function basename($file_name, $check_exists = true) {
+		public static function basename(string $file_name, bool $check_exists = true) {
 
 			return self::getInfo($file_name, PATHINFO_BASENAME, $check_exists);
 		}
 
 		# Get file filename
 
-		public static function filename($file_name, $check_exists = true) {
+		public static function filename(string $file_name, bool $check_exists = true) {
 
 			return self::getInfo($file_name, PATHINFO_FILENAME, $check_exists);
 		}
 
 		# Get file extension
 
-		public static function extension($file_name, $check_exists = true) {
+		public static function extension(string $file_name, bool $check_exists = true) {
 
 			return self::getInfo($file_name, PATHINFO_EXTENSION, $check_exists);
 		}
 
 		# Get file contents
 
-		public static function contents($file_name) {
+		public static function contents(string $file_name) {
 
-			$file_name = strval($file_name);
-
-			return ((false !== ($contents = @file_get_contents($file_name))) ? $contents : false);
+			return @file_get_contents($file_name);
 		}
 
 		# Get PHP file data
 
-		public static function php($file_name) {
+		public static function php(string $file_name) {
 
 			if ((strtolower(self::extension($file_name)) !== 'php')) return false;
 
@@ -153,7 +131,7 @@ namespace {
 
 		# Get XML file data
 
-		public static function xml($file_name) {
+		public static function xml(string $file_name) {
 
 			if ((strtolower(self::extension($file_name)) !== 'xml')) return false;
 
@@ -162,7 +140,7 @@ namespace {
 
 		# Get JSON file data
 
-		public static function json($file_name) {
+		public static function json(string $file_name) {
 
 			if ((strtolower(self::extension($file_name)) !== 'json')) return false;
 
@@ -171,15 +149,11 @@ namespace {
 
 		# Save data to file
 
-		public static function save($file_name, $contents, $force = false) {
-
-			$file_name = strval($file_name); $contents = strval($contents); $force = boolval($force);
+		public static function save(string $file_name, string $contents, bool $force = false) {
 
 			if (self::isFile($file_name)) if (!$force) return false; else if (!@unlink($file_name)) return false;
 
-			# ------------------------
-
-			return file_put_contents($file_name, $contents);
+			return @file_put_contents($file_name, $contents);
 		}
 	}
 }
