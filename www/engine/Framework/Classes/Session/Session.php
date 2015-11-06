69302ca5e3bab8c170adb0146cedcc49de54bffe
Anton Romanov <ant.web3@gmail.com>
2015-11-06 20:12:56 +0200

    Updated Session class

--- www/engine/Framework/Classes/Session/Session.php
+++ www/engine/Framework/Classes/Session/Session.php
@@ -6,19 +6,17 @@ namespace {
 
 		# Start session
 
-		public static function start($name, $lifetime) {
+		public static function start(string $name, int $lifetime) {
 
 			if (session_id()) return true;
 
-			$name = strval($name); $lifetime = intabs($lifetime);
-
 			ini_set('session.gc_maxlifetime', $lifetime);
 
-			ini_set('session.cache_expire', 0); ini_set('session.cache_limiter', 'nocache');
+			session_name($name); session_set_cookie_params($lifetime, '/');
 
 			session_cache_expire(0); session_cache_limiter('nocache');
 
-			session_name($name); session_set_cookie_params($lifetime, '/');
+
 
 			# ------------------------
 
@@ -34,36 +32,28 @@ namespace {
 
 		# Check if variable exists
 
-		public static function exists($name) {
-
-			$name = strval($name);
+		public static function exists(string $name) {
 
 			return (session_id() && isset($_SESSION[$name]));
 		}
 
 		# Get variable
 
-		public static function get($name) {
-
-			$name = strval($name);
+		public static function get(string $name) {
 
 			return ((session_id() && isset($_SESSION[$name])) ? $_SESSION[$name] : false);
 		}
 
 		# Set variable
 
-		public static function set($name, $value) {
-
-			$name = strval($name);
+		public static function set(string $name, $value) {
 
 			if (session_id()) $_SESSION[$name] = $value;
 		}
 
 		# Delete variable
 
-		public static function delete($name) {
-
-			$name = strval($name);
+		public static function delete(string $name) {
 
 			if (session_id() && isset($_SESSION[$name])) unset($_SESSION[$name]);
 		}
