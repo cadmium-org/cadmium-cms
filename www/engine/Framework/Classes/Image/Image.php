405e7d37a95b592fcdde15b786d08ffe71820a85
Anton Romanov <ant.web3@gmail.com>
2015-11-07 19:06:11 +0200

    Added Image class

---
+++ www/engine/Framework/Classes/Image/Image.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace {
+
+	abstract class Image {
+
+		private static function output($image, string $mime, callable $outputter, array $params) {
+
+			if (!is_resource($this->captcha)) return false;
+
+			Headers::nocache(); Headers::content($mime);
+
+			call_user_func($outputter, ...$params); imagedestroy($image);
+
+			# ------------------------
+
+			return true;
+		}
+
+		# Output JPEG image
+
+		public static function outputJPEG($image, int $quality = 75) {
+
+			$params = [null, Number::format($quality, 0, 100)];
+
+			return self::output($image, MIME_TYPE_JPEG, 'imagejpeg', $params);
+		}
+
+		# Output PNG image
+
+		public static function outputPNG($image, int $quality = 4) {
+
+			$params = [null, Number::format($quality, 0, 9)];
+
+			return self::output($image, MIME_TYPE_PNG, 'imagepng', $params);
+		}
+
+		# Output GIF image
+
+		public static function outputGIF($image) {
+
+			return self::output($image, MIME_TYPE_GIF, 'imagegif');
+		}
+	}
+}
