4ea9f6ac17c8df51105f78605654343896e1efc9
Anton Romanov <ant.web3@gmail.com>
2015-11-16 01:49:44 +0200

    Updated System Installer

--- www/engine/System/Classes/Installer.php
+++ www/engine/System/Classes/Installer.php
@@ -10,6 +10,12 @@ namespace System {
 
 		public function handle() {
 
+			# Check installation
+
+			// if (!$this->installed) Request::redirect(INSTALL_PATH . '/index.php');
+
+			# Determine handler class
+
 			$checked = (Install::status() && boolval(Request::get('checked')));
 
 			$class = ('System\Handlers\Admin\Install\\' . (!$checked ? 'Check' : 'Database'));
