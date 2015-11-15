58104ce125213b5da9681b9b03d939abcbe03a8f
Anton Romanov <ant.web3@gmail.com>
2015-11-15 21:05:53 +0200

    Updated System Captcha class

--- www/engine/System/Classes/Utils/Tools/Captcha.php
+++ www/engine/System/Classes/Utils/Tools/Captcha.php
@@ -2,52 +2,46 @@
 
 namespace System\Utils\Tools {
 
-	use Explorer, Headers, Number;
+	use Explorer, Number;
 
 	class Captcha {
 
-		private $captcha = null, $width = 0, $height = 0;
+		private $image = null, $width = 0, $height = 0;
 
 		# Allocate color
 
-		private function allocateColor($color) {
+		private function allocateColor(array $color = null) {
 
-			if (!is_resource($this->captcha)) return false;
+			if (!is_resource($this->image)) return false;
 
-			if (null === $color) $color = [0, 0, 0]; else if (!is_array($color) || (count($color) !== 3)) return false;
+			if (null === $color) $color = [0, 0, 0]; else if (count($color) !== 3) return false;
 
 			foreach (array_values($color) as $key => $value) $color[$key] = Number::format($value, 0, 255);
 
 			# ------------------------
 
-			return imagecolorallocate($this->captcha, $color[0], $color[1], $color[2]);
+			return imagecolorallocate($this->image, ...$color);
 		}
 
 		# Constructor
 
-		public function __construct($width, $height, $bg_color = null) {
+		public function __construct(int $width, int $height, array $bg_color = null) {
 
-			$width = intval($width); $height = intval($height);
+			if (false === ($image = imagecreatetruecolor($width, $height))) return;
 
-			if (false === ($captcha = imagecreatetruecolor($width, $height))) return;
-
-			$this->captcha = $captcha; $this->width = $width; $this->height = $height;
+			$this->image = $image; $this->width = $width; $this->height = $height;
 
 			if (false !== ($bg_color = $this->allocateColor($bg_color))) {
 
-				imagefilledrectangle($captcha, 0, 0, ($width - 1), ($height - 1), $bg_color);
+				imagefilledrectangle($image, 0, 0, ($width - 1), ($height - 1), $bg_color);
 			}
 		}
 
 		# Draw text
 
-		public function text($font, $size, $indent, $step, $text, $color = null) {
+		public function text(string $font, int $size, int $indent, int $step, string $text, array $color = null) {
 
-			if (!is_resource($this->captcha)) return false;
-
-			$font = strval($font); $size = ((($size = intval($size)) < 8) ? 8 : $size);
-
-			$indent = intval($indent); $step = intval($step); $text = strval($text);
+			if (!is_resource($this->image)) return false;
 
 			if (!Explorer::isFile($font)) return false;
 
@@ -57,9 +51,9 @@ namespace System\Utils\Tools {
 
 			foreach (str_split($text) as $index => $character) {
 
-				$angle = mt_rand(-10, 10); $x = ($indent + ($index * $step)); $y = mt_rand(($base - 5), ($base + 5));
+				$angle = random_int(-10, 10); $x = ($indent + ($index * $step)); $y = random_int(($base - 5), ($base + 5));
 
-				imagettftext($this->captcha, $size, $angle, $x, $y, $color, $font, $character);
+				imagettftext($this->image, $size, $angle, $x, $y, $color, $font, $character);
 			}
 
 			# ------------------------
@@ -69,20 +63,15 @@ namespace System\Utils\Tools {
 
 		# Draw noise
 
-		public function noise($rate, $color = null) {
+		public function noise(int $rate, array $color = null) {
 
-			if (!is_resource($this->captcha)) return false;
-
-			$rate = intval($rate);
+			if (!is_resource($this->image)) return false;
 
 			if (false === ($color = $this->allocateColor($color))) return false;
 
-			for ($x = 0; $x < $this->width; $x++) {
+			for ($x = 0; $x < $this->width; $x++) for ($y = 0; $y < $this->height; $y++) {
 
-				for ($y = 0; $y < $this->height; $y++) {
-
-					if (mt_rand(1, $rate) === 1) imagesetpixel($this->captcha, $x, $y, $color);
-				}
+				if (random_int(1, $rate) === 1) imagesetpixel($this->image, $x, $y, $color);
 			}
 
 			# ------------------------
@@ -92,21 +81,19 @@ namespace System\Utils\Tools {
 
 		# Draw lines
 
-		public function lines($count, $color = null) {
+		public function lines(int $count, array $color = null) {
 
-			if (!is_resource($this->captcha)) return false;
-
-			$count = intval($count);
+			if (!is_resource($this->image)) return false;
 
 			if (false === ($color = $this->allocateColor($color))) return false;
 
 			for ($i = 0; $i < $count; $i++) {
 
-				$point_0 = [0, mt_rand(0, ($this->height - 1))];
+				$point_0 = [0, random_int(0, ($this->height - 1))];
 
-				$point_1 = [($this->width - 1), mt_rand(0, ($this->height - 1))];
+				$point_1 = [($this->width - 1), random_int(0, ($this->height - 1))];
 
-				imageline($this->captcha, $point_0[0], $point_0[1], $point_1[0], $point_1[1], $color);
+				imageline($this->image, $point_0[0], $point_0[1], $point_1[0], $point_1[1], $color);
 			}
 
 			# ------------------------
@@ -114,21 +101,11 @@ namespace System\Utils\Tools {
 			return true;
 		}
 
-		# Output PNG image
+		# Return image
 
-		public function output($quality = 4) {
+		public function image() {
 
-			if (!is_resource($this->captcha)) return false;
-
-			if (($quality = intval($quality)) > 9) $quality = 9;
-
-			Headers::nocache(); Headers::content(MIME_TYPE_PNG);
-
-			imagepng($this->captcha, null, $quality); imagedestroy($this->captcha);
-
-			# ------------------------
-
-			return true;
+			return $this->image;
 		}
 	}
 }
