7c6f5730fc6afa201ac09e051580eda059c6c522
Anton Romanov <ant.web3@gmail.com>
2015-11-25 17:56:00 +0200

    Added comment

--- www/engine/System/Classes/Modules/Install/Controller/Database.php
+++ www/engine/System/Classes/Modules/Install/Controller/Database.php
@@ -10,6 +10,8 @@ namespace System\Modules\Install\Controller {
 
 		public function __invoke(array $post) {
 
+			# Declare variables
+
 			$server = ''; $user = ''; $password = ''; $name = '';
 
 			# Extract post array
