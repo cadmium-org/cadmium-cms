012efb3743749c5b2520f85aad071e080c78acc7
Anton Romanov <ant.web3@gmail.com>
2017-01-07 18:52:38 +0200

    Update Schemas package docs

--- www/engine/System/Classes/Schemas/Prototype/Language.php
+++ www/engine/System/Classes/Schemas/Prototype/Language.php
@@ -1,12 +1,21 @@
 <?php
 
+/**
+ * @package Cadmium\System\Schemas
+ * @author Anton Romanov
+ * @copyright Copyright (c) 2015-2017, Anton Romanov
+ * @link http://cadmium-cms.com
+ */
+
 namespace Schemas\Prototype {
 
 	use Utils\Schema;
 
 	class Language extends Schema\_Object {
 
-		# Constructor
+		/**
+		 * Constructor
+		 */
 
 		public function __construct() {
 
