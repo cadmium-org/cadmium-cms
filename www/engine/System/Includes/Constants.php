6df8b80bbe63ea2468ea58e3b622edeca1610d03
Anton Romanov <ant.web3@gmail.com>
2017-01-28 01:32:28 +0200

    Update CKEditor to 4.6.2

--- www/engine/System/Includes/Constants.php
+++ www/engine/System/Includes/Constants.php
@@ -19,7 +19,7 @@ define('CADMIUM_COPY',                              '2017');
 
 define('JQUERY_VERSION',                            '3.1.1');
 define('SEMANTIC_UI_VERSION',                       '2.2.7');
-define('CKEDITOR_VERSION',                          '4.6.0');
+define('CKEDITOR_VERSION',                          '4.6.2');
 
 # Sections
 
