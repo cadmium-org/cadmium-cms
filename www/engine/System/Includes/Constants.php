854bbe2db5fee288ddca91213260339e16baaef9
Anton Romanov <ant.web3@gmail.com>
2017-01-28 01:25:17 +0200

    Update Semantic UI to 2.2.7

--- www/engine/System/Includes/Constants.php
+++ www/engine/System/Includes/Constants.php
@@ -18,7 +18,7 @@ define('CADMIUM_COPY',                              '2017');
 # Third-party
 
 define('JQUERY_VERSION',                            '3.1.1');
-define('SEMANTIC_UI_VERSION',                       '2.2.6');
+define('SEMANTIC_UI_VERSION',                       '2.2.7');
 define('CKEDITOR_VERSION',                          '4.6.0');
 
 # Sections
