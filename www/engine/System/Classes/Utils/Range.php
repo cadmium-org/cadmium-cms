8abd4671de8b1a139485016965dba898176ff66a
Anton Romanov <ant.web3@gmail.com>
2017-06-15 20:55:28 +0300

    Update Range utility docs

--- www/engine/System/Classes/Utils/Range.php
+++ www/engine/System/Classes/Utils/Range.php
@@ -1,26 +1,43 @@
 <?php
 
+/**
+ * @package Cadmium\System\Utils
+ * @author Anton Romanov
+ * @copyright Copyright (c) 2015-2017, Anton Romanov
+ * @link http://cadmium-cms.com
+ */
+
 namespace Utils {
 
 	use Language;
 
 	abstract class Range extends \Range {
 
-		# Translate value
+		/**
+		 * Translate a value accordingly to an active language
+		 *
+		 * @return string|mixed : the translated value or the raw value if the phrase does not exist
+		 */
 
 		private static function translate($value) {
 
 			return ((false !== ($translated = Language::get($value))) ? $translated : $value);
 		}
 
-		# Get item by key
+		/**
+		 * Get a translated item value by a key
+		 *
+		 * @return mixed|null : the value if the key exists, otherwise null
+		 */
 
 		public static function get($key) {
 
 			return ((null !== ($value = parent::get($key))) ? self::translate($value) : null);
 		}
 
-		# Get range array
+		/**
+		 * Get the translated range array
+		 */
 
 		public static function getRange() : array {
 
--- www/engine/System/Classes/Utils/Range/Rank.php
+++ www/engine/System/Classes/Utils/Range/Rank.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @package Cadmium\System\Utils
+ * @author Anton Romanov
+ * @copyright Copyright (c) 2015-2017, Anton Romanov
+ * @link http://cadmium-cms.com
+ */
+
 namespace Utils\Range {
 
 	use Utils\Range;
