07211f4bee2babf4c56a8772e1531dbdbe3941fe
Anton Romanov <ant.web3@gmail.com>
2017-10-02 16:25:57 +0300

    Change version to 0.4.9

--- www/engine/System/Includes/Constants.php
+++ www/engine/System/Includes/Constants.php
@@ -12,7 +12,7 @@
 define('CADMIUM_NAME',                              'Cadmium CMS');
 define('CADMIUM_HOME',                              'http://cadmium-cms.com');
 
-define('CADMIUM_VERSION',                           '0.4.8');
+define('CADMIUM_VERSION',                           '0.4.9');
 define('CADMIUM_COPY',                              '2017');
 
 # Third-party
